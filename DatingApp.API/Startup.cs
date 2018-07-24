1f1725a1204315a21f2c98280e2dd6204008db9c
Mono Noor Fadhilah <halilintar@gmail.com>
2018-07-24 15:09:41 +0800

    Add project files.

---
+++ DatingApp.API/Startup.cs
@@ -0,0 +1,50 @@
+﻿using DatingApp.API.Data;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DatingApp.API
+{
+  public class Startup
+  {
+    public Startup(IConfiguration configuration)
+    {
+      Configuration = configuration;
+    }
+
+    public IConfiguration Configuration { get; }
+
+    // This method gets called by the runtime. Use this method to add services to the container.
+    public void ConfigureServices(IServiceCollection services)
+    {
+      services.AddDbContext<DataContext>(opt => opt.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
+
+      services.AddCors();
+
+      services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+    }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+    {
+      if (env.IsDevelopment())
+      {
+        app.UseDeveloperExceptionPage();
+      }
+      else
+      {
+        //app.UseHsts();
+      }
+
+      app.UseCors(opt => opt.AllowAnyHeader()
+        .AllowAnyMethod()
+        .AllowAnyOrigin());
+
+      //app.UseHttpsRedirection();
+      app.UseMvc();
+    }
+  }
+}
