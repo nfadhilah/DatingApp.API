659d810e0a1e065cd2acaed9c12f496567f13ff0
Mono Noor Fadhilah <halilintar@gmail.com>
2018-07-24 18:57:24 +0800

    Implementing Security

--- DatingApp.API/Controllers/ValuesController.cs
+++ DatingApp.API/Controllers/ValuesController.cs
@@ -1,10 +1,12 @@
 ﻿using DatingApp.API.Data;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System.Threading.Tasks;
 
 namespace DatingApp.API.Controllers
 {
+  [Authorize]
   [Route("api/[controller]")]
   [ApiController]
   public class ValuesController : ControllerBase
@@ -16,7 +18,6 @@ namespace DatingApp.API.Controllers
       _context = context;
     }
 
-    // GET api/values
     [HttpGet]
     public async Task<IActionResult> Get()
     {
@@ -25,7 +26,7 @@ namespace DatingApp.API.Controllers
       return Ok(values);
     }
 
-    // GET api/values/5
+    [AllowAnonymous]
     [HttpGet("{id}")]
     public async Task<IActionResult> Get(int id)
     {
---
+++ DatingApp.API/Dtos/UserForRegisterDto.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DatingApp.API.Dtos
+{
+  public class UserForRegisterDto
+  {
+    [Required]
+    public string Username { get; set; }
+    [Required]
+    [StringLength(10, MinimumLength = 6, ErrorMessage = "You must specify password between 6 and 10 characters")]
+    public string Password { get; set; }
+  }
+}
