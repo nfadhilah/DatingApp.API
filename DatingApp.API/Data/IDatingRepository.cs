50470eb0382ec128d67c6d1846d964988ece4139
Mono Noor Fadhilah <halilintar@gmail.com>
2018-08-09 13:44:49 +0800

    End of Section 16

--- DatingApp.API/Data/IDatingRepository.cs
+++ DatingApp.API/Data/IDatingRepository.cs
@@ -1,9 +1,6 @@
 ﻿using DatingApp.API.Helpers;
 using DatingApp.API.Models;
-using Microsoft.EntityFrameworkCore;
-using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace DatingApp.API.Data
@@ -18,112 +15,8 @@ namespace DatingApp.API.Data
     Task<Photo> GetPhoto(int id);
     Task<Photo> GetMainPhotoForUser(int userId);
     Task<Like> GetLike(int userId, int recipientId);
-  }
-
-  public class DatingRepository : IDatingRepository
-  {
-    private readonly DataContext _context;
-
-    public DatingRepository(DataContext context)
-    {
-      _context = context;
-    }
-
-    public void Add<T>(T entity) where T : class
-    {
-      _context.Add(entity);
-    }
-
-    public void Delete<T>(T entity) where T : class
-    {
-      _context.Remove(entity);
-    }
-
-    public async Task<bool> SaveAll()
-    {
-      return await _context.SaveChangesAsync() > 0;
-    }
-
-    public async Task<PagedList<User>> GetUsers(UserParams userParams)
-    {
-      var users = _context.Users.Include(p => p.Photos)
-        .OrderByDescending(u => u.LastActive)
-        .AsQueryable();
-
-      users = users.Where(u => u.Id != userParams.UserId);
-
-      users = users.Where(u => u.Gender == userParams.Gender);
-
-      if (userParams.Likers)
-      {
-        var userLikers = await GetUserLikes(userParams.UserId, userParams.Likers);
-        users = users.Where(u => userLikers.Contains(u.Id));
-      }
-
-      if (userParams.Likees)
-      {
-        var userLikees = await GetUserLikes(userParams.UserId, userParams.Likers);
-        users = users.Where(u => userLikees.Contains(u.Id));
-      }
-
-      if (userParams.MinAge != 18 || userParams.MinAge != 99)
-      {
-        var minDob = DateTime.Today.AddYears(-userParams.MaxAge - 1);
-        var maxDob = DateTime.Today.AddYears(-userParams.MinAge);
-
-        users = users.Where(u => u.DateOfBirth >= minDob && u.DateOfBirth <= maxDob);
-      }
-
-      if (!string.IsNullOrWhiteSpace(userParams.OrderBy))
-      {
-        switch (userParams.OrderBy)
-        {
-          case "created":
-            users = users.OrderByDescending(u => u.Created);
-            break;
-          default:
-            users = users.OrderByDescending(u => u.LastActive);
-            break;
-        }
-      }
-
-      return await PagedList<User>.CreateAsync(users, userParams.PageNumber, userParams.PageSize);
-    }
-
-    private async Task<IEnumerable<int>> GetUserLikes(int id, bool likers)
-    {
-      var user = await _context.Users
-        .Include(x => x.Likees).Include(x => x.Likers)
-        .FirstOrDefaultAsync(u => u.Id == id);
-
-      return likers ? user.Likers.Where(u => u.LikeeId == id).Select(u => u.LikerId) :
-        user.Likees.Where(u => u.LikerId == id).Select(u => u.LikeeId);
-    }
-
-    public async Task<User> GetUser(int id)
-    {
-      var user = await _context.Users.Include(p => p.Photos)
-        .FirstOrDefaultAsync(u => u.Id == id);
-
-      return user;
-    }
-
-    public async Task<Photo> GetPhoto(int id)
-    {
-      var photo = await _context.Photos.FirstOrDefaultAsync(p => p.Id == id);
-
-      return photo;
-    }
-
-    public async Task<Photo> GetMainPhotoForUser(int userId)
-    {
-      return await _context.Photos.Where(u => u.UserId == userId).FirstOrDefaultAsync(p => p.IsMain);
-    }
-
-    public async Task<Like> GetLike(int userId, int recipientId)
-    {
-      return await _context.Likes
-        .FirstOrDefaultAsync(u => u.LikerId == userId && u.LikeeId == recipientId);
-    }
+    Task<Message> GetMessage(int id);
+    Task<PagedList<Message>> GetMessagesForUser(MessageParams messageParams);
+    Task<IEnumerable<Message>> GetMessageThread(int userId, int recipientId);
   }
 }
